3e56a7cae3bc7a100f5f3709b4ffacb1e9a89b47
Patrick Molgaard <patrick@gleepost.com>
2015-11-26 16:08:31 +0000

    Beginnings of a config->setup button

---
+++ app/code/local/LoyaltyLion/CouponImport/Block/Adminhtml/Button.php
@@ -0,0 +1,50 @@
+<?php
+class LoyaltyLion_CouponImport_Block_Adminhtml_Button extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    /*
+     * Set template
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('loyaltylion/system/config/button.phtml');
+    }
+ 
+    /**
+     * Return element html
+     *
+     * @param  Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        return $this->_toHtml();
+    }
+ 
+    /**
+     * Return ajax url for button
+     *
+     * @return string
+     */
+    public function getAjaxCheckUrl()
+    {
+        return Mage::helper('adminhtml')->getUrl('adminhtml/adminhtml_loyaltylioncouponimport/setup');
+    }
+ 
+    /**
+     * Generate button html
+     *
+     * @return string
+     */
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setData(array(
+            'id'        => 'loyaltylion_setup_button',
+            'label'     => $this->helper('adminhtml')->__('Quick-Setup API access'),
+            'onclick'   => 'javascript:doSetup(); return false;'
+        ));
+ 
+        return $button->toHtml();
+    }
+}
